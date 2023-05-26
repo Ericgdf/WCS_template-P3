d683abdf55b710e044b8deda52fff16657ab4433
Eric Godefroy <ericgodefroy@outlook.fr>
2023-05-26 12:15:48 +0200

    Version route

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json()); // parse la donnée en .json reçus de l'APi (la req)
+
+const APIRouter = express.Router();
+
+APIRouter.get('/version', function(req, res){
+    const {version} = require('./package.json') // NORME récupère la version
+    return res.json({version}) // la renvoie en format json  Les standard de version : coorection des bug dernier chiffre / deuxieme chiffre ajouts d'une nouvelle feature / premier chiffres breaking change 
+})
+
+
+app.use('/api',APIRouter);
+
+
+
+app.listen(8080, function(){
+    console.log('API is running on 8080')
+}) // app écoute sur le port 8080
